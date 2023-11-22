cf435cb8293f95ae0345f695dc96af90c42a0eaf
siddharaj-sinh <siddharajsinh@promactinfo.com>
2023-11-22 14:24:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            var configuration = builder.Configuration;
+            builder.Configuration.AddEnvironmentVariables();
+
+            builder.Services.AddControllers();
+            var temp1 = configuration["Demo:Key1"];
+            Console.WriteLine(temp1);
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
